694f8fa27ed5dcffcef7a25b036a61c1a1d7fdde
Gabriel Santos / yKold <108306401+yKold@users.noreply.github.com>
2024-06-17 15:03:50 -0300

    deu certo

--- EstudosAngular/src/app/app.routes.ts
+++ EstudosAngular/src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
+import { CreateProductsComponent } from './features/create-products/create-products.component';
 
-export const routes: Routes = [{
-    path: "",
-    component: ListComponent
-}];
+export const routes: Routes = [
+    {
+        path: "",
+        component: ListComponent
+    },
+    {
+        path: "create-product",
+        component: CreateProductsComponent
+    }
+];
