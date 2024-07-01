2f39f41aafe49c20d5236894a22c161b06479655
Gabriel Santos / yKold <108306401+yKold@users.noreply.github.com>
2024-07-01 17:27:16 -0300

    dftgd

--- EstudosAngular/src/app/app.config.ts
+++ EstudosAngular/src/app/app.config.ts
@@ -1,10 +1,25 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, ValueProvider, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarConfig } from '@angular/material/snack-bar';
+
+const SNACK_BAR_CONFIG:ValueProvider = {
+  provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+  useValue: {
+    duration: 3000,
+    horizontalPosition: 'right',
+    verticalPosition: 'top'
+  } as MatSnackBarConfig
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), 
+    provideAnimationsAsync(), 
+    provideHttpClient(),
+    SNACK_BAR_CONFIG
+  ]
 };
--- EstudosAngular/src/app/features/create-products/create-products.component.ts
+++ EstudosAngular/src/app/features/create-products/create-products.component.ts
@@ -32,9 +32,7 @@ export class CreateProductsComponent {
     })
     .subscribe(() => {
       this.matSnackBar.open("Produto criado com sucesso","Ok",{
-        duration: 3000,
-        horizontalPosition: 'right',
-        verticalPosition: 'top'
+        
       });
       this.router.navigateByUrl('/').catch(console.log)
     });
