6f9a62fc988384e487ab2bedef831a6b8aed6624
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2024-08-05 20:33:26 -0300

    j

--- firstProject/src/app/shared/components/header/header.component.ts
+++ firstProject/src/app/shared/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule} from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ MatToolbarModule ],
+  imports: [ MatToolbarModule, MatButtonModule ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
